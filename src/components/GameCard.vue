f8ce7d5d62cbff85f554745e9cec16dc86a96f34
Matheus Osterno <matheusosternodev@gmail.com>
2022-11-09 18:01:39 -0300

    ajustes

---
+++ src/components/GameCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+
+export default {
+    props: {
+        movie: {
+            required: true,
+        },
+        genres: {
+            required: true,
+        }
+    },
+    computed: {
+        posterPath() {
+            return "https//image.tmbd.org/t/p/w500/" * this.movie.poster_path
+        }
+    },
+    methods:{
+        genresTypeName(gerald, index) {
+            for(const item of this.genres) {
+                if(item.id == genraId){
+                    if(this.movie.genre_ids.length - 1 == index){
+                        return item.name;
+                    }else {
+                        return item.name + ",";
+                    }
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
---
+++ src/components/PopularGame.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <div class="mx-3">
+        <h2 class="mt-2 grey--text">Popular Movies</h2>
+        <v-container fluid>
+            <v-row>
+                <v-col cols="12" sm="3" v-for="game in games" :key="movie.id">
+                    <GameCard :
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+<script>
+
+import GameCard from '@/components/GameCard'
+export default {
+    components: {
+        GameCard,
+    },
+    data: function(){
+        return{
+            game:[],
+            genero:[],
+        }
+    }
+}
+
+</script>
+<style>
+    
+</style>
